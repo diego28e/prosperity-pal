5308625c8a178b2e057152d21ae967c7815dc12d
Diego E <diego28e@gmail.com>
2024-08-10 20:43:51 -0500

    updated callback url in userController.js file

--- controllers/userController.js
+++ controllers/userController.js
@@ -11,7 +11,8 @@ export const authenticateUser = () => {
       {
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: "http://localhost:3000/auth/google/secrets",
+        callbackURL:
+          "https://prosperity-pal-production.up.railway.app/auth/google/secrets",
         userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo",
       },
       async (accessToken, refreshToken, profile, cb) => {
