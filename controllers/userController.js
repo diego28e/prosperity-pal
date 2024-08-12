c2139815436962ac5fbac461cdd86d8cbf7c5f37
Diego E <diego28e@gmail.com>
2024-08-11 22:38:28 -0500

    improved responsiveness of app

--- controllers/userController.js
+++ controllers/userController.js
@@ -15,7 +15,7 @@ export const authenticateUser = () => {
       {
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: "https://academiadelcodigo.com/auth/google/secrets",
+        callbackURL: `${process.env.DOMAIN}/auth/google/secrets`,
         userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo",
       },
       async (accessToken, refreshToken, profile, cb) => {
